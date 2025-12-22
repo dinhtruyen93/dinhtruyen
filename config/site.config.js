a84a0179dbe9885aa4c99779fc6102c96698f895
dinhtruyen93 <121920055+dinhtruyen93@users.noreply.github.com>
2025-12-22 22:15:40 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -40,7 +40,7 @@ module.exports = {
   // the directories in which you have .password set. Check the documentation for details.
 "protectedRoutes": [
   "/Revit BIM",
-    "/Film",
+     "/Film",
 ],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
