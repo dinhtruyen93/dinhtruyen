2e071020987d0224ce589bc5f311fa723d8b8913
dinhtruyen93 <121920055+dinhtruyen93@users.noreply.github.com>
2025-12-23 11:00:02 +0700

    Update site.config.js
    base

--- config/site.config.js
+++ config/site.config.js
@@ -38,7 +38,10 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-"protectedRoutes": ["/Revit BIM", "/Film",],
+"protectedRoutes": [
+  "/Revit BIM",
+  "/Film",
+],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:dinhtruyen.pham@hotmail.com',
