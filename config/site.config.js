01f5831f290e2771a8a8048d4da9ea982921a0e2
dinhtruyen93 <121920055+dinhtruyen93@users.noreply.github.com>
2025-12-23 10:29:02 +0700

    Update site.config.js
    update vesion 2

--- config/site.config.js
+++ config/site.config.js
@@ -7,7 +7,7 @@ module.exports = {
   // Make sure this is exactly the same as the email address you use to sign into your Microsoft account.
   // You can also put this in your worker's environment variable if you worry about
   // your email being exposed in public.
-  userPrincipalName: process.env.USER_PRINCIPLE_NAME || 'dinhtruyen.pham@gmail.com',
+  userPrincipalName: process.env.USER_PRINCIPLE_NAME || 'dinhtruyen.pham@hotmail.com',
 
   // [OPTIONAL] This is the website icon to the left of the title inside the navigation bar. It should be placed under the
   // /public directory of your GitHub project (not your OneDrive folder!), and referenced here by its relative path to /public.
@@ -34,11 +34,12 @@ module.exports = {
 
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
-
+  footer:
+    'Powered by <a href="https://github.com/lyc8503/onedrive-cf-index-ng" target="_blank" rel="noopener noreferrer">onedrive-cf-index-ng</a>. Made with ❤',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-"protectedRoutes": ['/Revit BIM', '/Film',],
+  protectedRoutes: ['/Revit BIM', '/Film',],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:dinhtruyen.pham@hotmail.com',
